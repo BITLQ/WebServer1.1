7b6188edbfc5599e2f6a2866d782e3d0a951409e
Johnxs <849886839@qq.com>
2017-08-15 18:01:02 +0800

    Update main.cpp

--- webserver1.1/main.cpp
+++ webserver1.1/main.cpp
@@ -3,9 +3,12 @@
 #define MAX_FD 65535
 #include<sys/epoll.h>
 #include<errno.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 #define FD_LIMIT 65535
 #define MAX_EVENT_NUMBER 1024
-#define TIMESLOT 20 
+#define TIMESLOT 10
 
 static int pipefd[2];
 static time_heap timer_heap(10);
@@ -43,10 +46,10 @@ private:
 void sig_handler(int sig)
 {
     int save_errno = errno;
-    printf("sig_handler %d is runnig\n", sig);
+//    printf("sig_handler %d is runnig\n", sig);
     int msg = sig;
     send(pipefd[1], (char*)&msg, 1, 0);
-    printf("sig_handler is runned\n");
+//   printf("sig_handler is runned\n");
     errno = save_errno;
 }
 
@@ -70,14 +73,14 @@ static void Useage(const char* str)
 void timer_handler()
 {
     timer_heap.tick();
-    alarm(TIMESLOT);
+    alarm(2);
 }
 
 void cb_func(client_data* user_data)
 {
-    printf("cb_func is running!\n");
+ //   printf("cb_func is running!\n");
     epoll_ctl(epollfd, EPOLL_CTL_DEL,user_data->sockfd, 0);
-    assert(user_data);
+    close(user_data->sockfd);
     printf("close fd %d\n",user_data->sockfd);
 }
 
@@ -89,7 +92,8 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-//	daemon(1,0);
+    //守护进程的方式运行
+	//daemon(1,0);
 	
 	int listensock = startup(argv[1],atoi(argv[2]));
     
@@ -105,11 +109,13 @@ int main(int argc, char* argv[])
     
     epoll_event events[MAX_EVENT_NUMBER];
     assert(epollfd != -1);
+    //modfd(epollfd, listensock);
     addfd(epollfd, listensock);
 
     int ret = socketpair(AF_UNIX, SOCK_STREAM, 0, pipefd);
     assert(ret != -1);
     setnonblocking(pipefd[1]);
+    //modfd(epollfd, pipefd[0]);
     addfd(epollfd, pipefd[0]);
 
 
@@ -121,14 +127,13 @@ int main(int argc, char* argv[])
 
     client_data* users = new client_data[FD_LIMIT];
     bool timeout = false;
-    alarm(TIMESLOT);  /* 定时 */
+    alarm(2);  /* 定时 */
 
     while(!stop_server)
     {
         int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
         if((number == -1))
         {
-            printf("epoll failed\n");
             continue;
         }
 
@@ -141,26 +146,30 @@ int main(int argc, char* argv[])
                 struct sockaddr_in client_address;
                 socklen_t client_addlenth = sizeof(client_address);
                 int connfd = accept(listensock, (struct sockaddr*)&client_address,&client_addlenth);
+                if(connfd >= FD_LIMIT)
+                {
+                    close(connfd);
+                    continue;
+                }
                 time_addfd(epollfd, connfd);
                 users[connfd].address = client_address;
                 users[connfd].sockfd = connfd;
 
                 /* 创建定时器，设置其回调函数与超时时间，然后绑定定时器与用户数据，最后将定时器添加到时间堆中； */
-                heap_timer* timer = new heap_timer(120);
+                heap_timer* timer = new heap_timer(TIMESLOT);
                 timer->user_data = &users[connfd];
+                users[connfd].timer = timer;
                 timer->cb_func = cb_func;
-                time_t cur = time(NULL);
-                timer->expire = cur + TIMESLOT;
                 timer_heap.add_timer(timer);
                 printf("new link add newfd %d!\n", connfd);
-                modfd(epollfd, sockfd);
+                //modfd(epollfd, sockfd);
             }
 
             /* 处理信号 */
             else if((sockfd == pipefd[0]) && (events[i].events & EPOLLIN))
             {
 
-                printf("deal signal");
+ //               printf("deal signal");
                 int sig;
                 char signals[1024];
                 ret = recv(pipefd[0], signals, sizeof(signals), 0);
@@ -175,10 +184,10 @@ int main(int argc, char* argv[])
                 }
                 else
                 {
-                    printf("signal ret is %d\n", ret);
+//                    printf("signal ret is %d\n", ret);
                     for(int i = 0; i < ret; i++)
                     {
-                        printf("signal is %d\n",signals[i]);
+ //                       printf("signal is %d\n",signals[i]);
                         switch(signals[i])
                         {
                             case SIGALRM:
@@ -199,13 +208,15 @@ int main(int argc, char* argv[])
             {
                 char c;
 			    int ret = recv(sockfd,&c,1,MSG_PEEK);
-                if(ret <= 0)
+                if(ret == 0 || (ret < 0 && errno != 11))
                 {
                     //关闭链接 
-                    cout<<"ret < 0"<<endl;
+                    cout<<"ret: "<<ret<<endl;
                     timer_heap.del_timer(users[sockfd].timer);
-                    //cb_func(&users[sockfd]);
+                  //  if(users[sockfd].timer->cb_func != NULL)
+                  //      cb_func(&users[sockfd]);
                     epoll_ctl(epollfd, EPOLL_CTL_DEL,sockfd, 0);
+                    close(sockfd);
                     users[sockfd].timer = NULL;
                 }
                 else
@@ -214,24 +225,25 @@ int main(int argc, char* argv[])
 
                      if(timer)
                      {
-                         heap_timer* tmp = NULL;
-                         tmp->expire = time(NULL) + TIMESLOT;
-                         tmp->cb_func = timer->cb_func;
-                         tmp->user_data = timer->user_data;
-
-                         timer_heap.del_timer(timer);
-                         printf("adjust timer once\n");
-                         users[sockfd].timer = tmp;
-                         timer_heap.add_timer(tmp);
+                      //   heap_timer* tmp = new heap_timer(TIMESLOT);
+                      //   tmp->cb_func = timer->cb_func;
+                      //   tmp->user_data = &users[sockfd];
+                      //   users[sockfd].timer = tmp;
+                      //   timer_heap.del_timer(timer);
+                      //   timer_heap.add_timer(tmp);
+                      timer->expire = time(NULL)+TIMESLOT;
                      }
-                    pool->append(&conn(sockfd));
+                    bool ret = pool->append(&conn(sockfd));
+                    if(ret == false)
+                    {
+                        continue;
+                    }
                 }
             }
         }
 
         if(timeout)
         {
-            printf("timer_handler is running");
             timer_handler();
             timeout = false;
         }
